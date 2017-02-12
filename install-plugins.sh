f598f9a2122293f9f8eb48393db0fdde4e80c1ec
Peter Thurner <p.thurner@blunix.org>
2017-02-12 02:52:33 +0100

    updated installer

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 #
-# Install the plugins
+# Quick and dirty installer
+# Will be replaced by proper .deb packaging
 
 set -e
-set -x
 
-mkdir -p /usr/lib/bloonix/etc/plugins/import/satellitesharing
-cd checks
+mkdir -vp /usr/lib/bloonix/etc/plugins/import/satellitesharing
+cd plugins
 
 # Iterate over all contained plugins
 for plugin in asn varnish4 php-fpm aws-instance docker du saltmaster-minions shorewall uwsgi ssllabs; do
 
+    echo "== Installing Plugin $plugin"
     # Install server components for the plugin
-    chmod 755 "check-${plugin}"
-    bloonix-create-plugin "../plugins/plugin-${plugin}" > "/usr/lib/bloonix/etc/plugins/import/satellitesharing/plugin-${plugin}"
+    chmod -v 755 "../checks/check-${plugin}"
+    ln -vsf "../checks/check-${plugin}" .
+    bloonix-create-plugin "plugin-${plugin}" > "/usr/lib/bloonix/etc/plugins/import/satellitesharing/plugin-${plugin}"
+    rm -v "check-${plugin}"
     bloonix-load-plugins -c /etc/bloonix/server/main.conf -p "/usr/lib/bloonix/etc/plugins/import/satellitesharing/plugin-${plugin}"
 
     # Install client components for the plugin
-    cp "check-${plugin}" /usr/lib/bloonix/plugins/
-    chmod 755 "/usr/lib/bloonix/plugins/check-${plugin}"
-
-    # Fix permisions
-    chown -R root:root /usr/lib/bloonix/
+    cp -v "../checks/check-${plugin}" /usr/lib/bloonix/plugins/
+    chmod -v 755 "/usr/lib/bloonix/plugins/check-${plugin}"
+    echo
 
 done
 
+echo "== Restarting bloonix server and webui"
 cd ..
-
+chown -R root:root /usr/lib/bloonix/
 service bloonix-server restart; service bloonix-webgui restart
 
